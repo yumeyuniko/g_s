86cf2acc2a7f6de9716d512fa7e709cce3f8884f
saori <daigocafe@msn.com>
2022-03-22 16:16:42 +0900

    components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
